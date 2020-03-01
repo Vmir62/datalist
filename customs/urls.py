5fd9e199f2c0c44dafe4484e057750890f1bedbe
Vmir62 <encantadomonte@gmail.com>
2020-03-01 18:59:04 +0300

    Defined class List in a view.py and urls

--- customs/urls.py
+++ customs/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .import views
+from .views import List
 
 
 urlpatterns = [
     path('', views.homepage),
+    path('l1/', List.as_view()),
 ]
--- customs/views.py
+++ customs/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class List(View):
+    def get(self, request):
+            return HttpResponse("This's a page of the customers_list one")
+
 def homepage(request):
-    return HttpResponse("This's a homepage of the data_list")
+    return HttpResponse("This's a homepage of the data_lists")
